f085035525ce7df8974a42f2c5cd33b7caccfdc3
ssgodbol <ssgodbol@ncsu.edu>
2018-09-20 02:59:20 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
@@ -11,6 +14,8 @@ import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 
 public class Player {
+	final static double sd = 9.99;
+
 	// create static instance for zookeeper class.
 	private static ZooKeeper zk;
 
@@ -20,6 +25,10 @@ public class Player {
 	// declare zookeeper instance to access ZooKeeper ensemble
 	private ZooKeeper zoo;
 	final CountDownLatch connectedSignal = new CountDownLatch(1);
+	static Stat stat;
+	static String pathScore = "/ZookeeperParentScore";
+	static String pathStatus = "/ZookeeperParentStatus";
+	static byte[] dataNode = "Zookeeper Dir for Player".getBytes();
 
 	public ZooKeeper connect(String host) throws IOException, InterruptedException {
 		zoo = new ZooKeeper(host, 5000, new Watcher() {
@@ -38,55 +47,117 @@ public class Player {
 		zoo.close();
 	}
 
-	// Method to create znode in zookeeper ensemble
+	// Method to create parent nodes
 	public static void create(String path, byte[] data) throws KeeperException, InterruptedException {
 		zk.create(path, data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
 	}
 
+	public static void createPlayerNodes(String path, byte[] data) throws KeeperException, InterruptedException {
+		zk.create(path, data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+	}
+
+	// Method to initialize Parent Score and Parent Status
+	public static void init() throws KeeperException, InterruptedException {
+
+		byte[] dataScore = "Zookeeper Parent Dir for Score".getBytes();
+		byte[] dataStatus = "Zookeeper Parent Dir for Status".getBytes();
+
+		//Parent Score Node
+		stat = zk.exists(pathScore, true);
+		if (stat != null) {
+			System.out.println("Node exists and the node version is " + stat.getVersion());
+		} else {
+			System.out.println("Creating Parent Score Node");
+			create(pathScore, dataScore);
+		}
+		//Parent Status Node
+		stat = zk.exists(pathStatus, true);
+		if (stat != null) {
+			System.out.println("Node exists and the node version is " + stat.getVersion());
+		} else {
+			System.out.println("Creating Parent Status Node");
+			create(pathStatus, dataStatus);
+		}
+	}
+
+	public static void playerOnline(String name) throws KeeperException, InterruptedException {
+		String nodeName = pathStatus + "/" + name;
+		stat = zk.exists(nodeName, true);
+		if (stat != null) {
+			System.out.println("Node already exists. Exiting...");
+			System.exit(0);
+		} else {
+			System.out.println("Creating Node "+nodeName);
+			create(nodeName, dataNode);
+		}
+	}
+
+	public static void playerOffline(String name) throws InterruptedException, KeeperException {
+		String nodeName = pathStatus + "/" + name;
+		zk.delete(nodeName, zk.exists(nodeName, true).getVersion());
+		System.out.println(name + " deleted");
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String path = "/ZookeeperParent";
 		String ip = args[0];
 		String name = args[1];
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				try {
+					playerOffline(name);
+					conn.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
 		try {
 			conn = new ZkConnect();
-			zk = conn.connect("localhost");
-			byte[] data = "Zookeeper Parent Dir".getBytes(); // Declare data
-			// create(path, data); // Create the data to the specified path
+			zk = conn.connect("localhost");// switch to IP
+			init();
+			playerOnline(name);
 
-			Stat stat = zk.exists(path, true); // Stat checks the path of the znode
-
-			if (stat != null) {
-				System.out.println("Node exists and the node version is " + stat.getVersion());
-			} else {
-				System.out.println("Node does not exists");
+			/*
+			 * Interactive Mode
+			 */
+			if (args.length < 3) {
+				System.out.println("Interactive Mode:");
+				Scanner sc = new Scanner(System.in);
+				while (sc.hasNext()) {
+					String currScore = sc.nextLine();
+					String nodeName = pathScore + "/" + name + ":" + currScore + ":";
+					createPlayerNodes(nodeName, currScore.getBytes());
+					System.out.println(nodeName);
+				}
+				sc.close();
+				playerOffline(name);
 			}
-
-			if (args[2] == null) {// Its player online post
-				// Update player status to online
-				
-
-			} else {// its a score post
+			/*
+			 * Automated Mode
+			 */
+			else {// its a score post
 
 				int count = Integer.parseInt(args[2]);
-				int delay = Integer.parseInt(args[2]);
-				int score = Integer.parseInt(args[2]);
-
-				int n = 0;
-				while (n < count) {
-					String node = path + "/" + name + n;
-
-					create(node, data);
-					System.out.println(node);
-					// delete children
-					// zk.delete(node, zk.exists(node, true).getVersion());
-					// System.out.println(node + " deleted");
-					n++;
+				int delay = Integer.parseInt(args[3]);
+				int score = Integer.parseInt(args[4]);
+				System.out.println("count = " + count + " " + "delay = " + delay + " " + " score = " + score);
+				Random r = new Random();
+				for (int n = 0; n < count; n++) {
+					// Do Normal distributon
+					int currScoreNum = (int) (r.nextGaussian() * sd + score);
+					String currScore = Integer.toString(currScoreNum);
+					//System.out.println(n);
+					String nodeName = pathScore + "/" + name + ":" + currScore + ":";
+					createPlayerNodes(nodeName, currScore.getBytes());
+					System.out.println(nodeName);
+					TimeUnit.SECONDS.sleep((long) (r.nextGaussian() + delay));
 				}
 			}
 			// delete parent node
 			// zk.delete(path, zk.exists(path, true).getVersion());
-			conn.close();
+
+			
 		} catch (Exception e) {
 			System.out.println(e.getMessage()); // Catch error message
 		}
